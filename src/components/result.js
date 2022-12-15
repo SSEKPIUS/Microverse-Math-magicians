b1cb79c7e8bcefce72faf5c0c5f9d7bf44f3e6f4
pius ssekweyama <ssekweyamapius@gmail.com>
2022-12-15 09:28:51 +0300

    resolve corrections

--- src/components/result.js
+++ src/components/result.js
@@ -3,10 +3,11 @@ import React from 'react';
 class result extends React.Component {
   constructor(props) {
     super(props);
+    this.state = null;
   }
 
   render() {
-    return <div className="item1 top">{this.props.Result.total ?? '0'}</div>;
+    return <div className="item1 top">{(this.props.Result.next ?? this.props.Result.total) ?? '0'}</div>;
   }
 }
 export default result;
