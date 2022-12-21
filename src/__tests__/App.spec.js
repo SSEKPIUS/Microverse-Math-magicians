a35a53fb9fc45936ed016ccb7248e4b23911c02b
Ermi <manermidem@gmail.com>
2022-12-21 17:46:40 +0300

    Modify App.spec.js

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -23,47 +23,65 @@ describe('Math Magicians: tests', () => {
       const wrapper = shallow(<App />);
       expect(toJson(wrapper)).toMatchSnapshot();
     });
+
     test('Error renders correctly enzyme', () => {
       const wrapper = shallow(<Error />);
       expect(toJson(wrapper)).toMatchSnapshot();
     });
+
     test('Home renders correctly enzyme', () => {
       const wrapper = shallow(<Home />);
       expect(toJson(wrapper)).toMatchSnapshot();
     });
+
     test('Key renders correctly enzyme', () => {
       const wrapper = shallow(<Key />);
       expect(toJson(wrapper)).toMatchSnapshot();
     });
+
     test('Quote renders correctly enzyme', () => {
       const wrapper = shallow(<Quote />);
       expect(toJson(wrapper)).toMatchSnapshot();
     });
   });
+
   describe('operate.js test', () => {
     test('AC', () => {
-      expect(calculate({ total: null, next: null, operation: null }, 'AC')).toMatchObject({ total: null, next: null, operation: null });
+      expect(
+        calculate({ total: null, next: null, operation: null }, 'AC'),
+      ).toMatchObject({ total: null, next: null, operation: null });
     });
+
     test('Addition', () => {
-      expect(calculate({ total: 4, next: 2, operation: '+' }, '=')).toMatchObject({ total: '6', next: null, operation: null });
+      expect(
+        calculate({ total: 4, next: 2, operation: '+' }, '='),
+      ).toMatchObject({ total: '6', next: null, operation: null });
     });
+
     test('Subtraction', () => {
-      expect(calculate({ total: 4, next: 2, operation: '-' }, '=')).toMatchObject({ total: '2', next: null, operation: null });
+      expect(
+        calculate({ total: 4, next: 2, operation: '-' }, '='),
+      ).toMatchObject({ total: '2', next: null, operation: null });
     });
   });
+
   describe('calculate.js test', () => {
     test('Addition', () => {
       expect(operate(4, 4, '+')).toBe('8');
     });
+
     test('Subtraction', () => {
       expect(operate(5, 4, '-')).toBe('1');
     });
+
     test('Multiplication', () => {
       expect(operate(5, 4, 'x')).toBe('20');
     });
+
     test('Division', () => {
       expect(operate(10, 2, '÷')).toBe('5');
     });
+
     test('Modulo', () => {
       expect(operate(10, 2, '%')).toBe('0');
     });
